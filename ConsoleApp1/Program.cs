60facf8748e3a56a66a30b9663282d19bddc8efd
Bekzat <75122487+bekzat@users.noreply.github.com>
2020-12-05 01:28:57 +0600

    update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,11 +6,38 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    /// <summary>
+    /// Test
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Возвращет сумму двух чисел
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        static int Sum(int a, int b)
+        {
+            int d = a + b;
+            return d;
+        }
+        /// <summary>
+        /// Возвращет сумму трех чисел
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        static int Sum(int a, int b, int c)
+        {
+            int d = a + b + c;
+            return d;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("HelloWorld");
+            int result = Sum(3, 5);
+            Console.WriteLine(result);
         }
     }
 }
