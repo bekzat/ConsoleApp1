58f14f70e12d3c114961a42447afdfe20966e59b
Bekzat <75122487+bekzat@users.noreply.github.com>
2020-12-06 00:10:31 +0600

    upd

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,33 +11,91 @@ namespace ConsoleApp1
     /// </summary>
     class Program
     {
-        /// <summary>
-        /// Возвращет сумму двух чисел
-        /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <returns></returns>
-        static int Sum(int a, int b)
+        static int[] fillInArray(int[] newArray)
         {
-            int d = a + b;
-            return d;
+            Random rnd = new Random();
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                newArray[i] = rnd.Next(100, 999);
+            }
+            return newArray;
         }
-        /// <summary>
-        /// Возвращет сумму трех чисел
-        /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <param name="c"></param>
-        /// <returns></returns>
-        static int Sum(int a, int b, int c)
+        static void printArray(int[] newArray)
         {
-            int d = a + b + c;
-            return d;
+            Console.WriteLine("массив:");
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                Console.Write(newArray[i]+"\t");
+            }
+            Console.WriteLine();
+        }
+        //HW 1
+        static int[] Resize(ref int[] newArray)
+        {
+            Console.Write("Размер массива: ");
+            int amount = int.Parse(Console.ReadLine());
+            newArray = new int[amount];
+            return newArray;
+        }
+
+        //HW 2
+        static ref int addFirstItem(int[] newArray)
+        {
+            return ref newArray[0];
+        }
+        static ref int addLastItem(int[] newArray)
+        {
+            return ref newArray[newArray.Length - 1];
+        }
+        static ref int addItem(int[] newArray)
+        {
+            Console.Write("Элемент под индексом: ");
+            int index = int.Parse(Console.ReadLine());
+            return ref newArray[index];
+        }
+
+        //HW 3
+        static ref int delFirstItem(int[] newArray)
+        {
+            return ref newArray[0];
+        }
+        static ref int delLastItem(int[] newArray)
+        {
+            return ref newArray[newArray.Length-1];
+        }
+        static ref int delItem(int[] newArray)
+        {
+            Console.Write("\nУдаление элемента под индексом: ");
+            int index = int.Parse(Console.ReadLine());
+            return ref newArray[index];
         }
         static void Main(string[] args)
-        {            
-            int result = Sum(3, 5);
-            Console.WriteLine($"Result= {result}");
+        {
+            int[] myArray = new int[0];
+            Resize(ref myArray);
+            fillInArray(myArray);
+            Console.WriteLine("Количество элементов массива = " + myArray.Length);
+            ref int firstItem = ref addFirstItem(myArray);
+            Console.Write("Изменить первый элемент: ");
+            firstItem = int.Parse(Console.ReadLine());
+            ref int lastItem = ref addLastItem(myArray);
+            Console.Write("Изменить последний элемент: ");
+            lastItem = int.Parse(Console.ReadLine());
+            ref int yourItem = ref addItem(myArray);
+            Console.Write("Изменить на: ");
+            yourItem = int.Parse(Console.ReadLine());
+            printArray(myArray);
+            Console.WriteLine();
+            ref int deleteFirstItem = ref delFirstItem(myArray);
+            Console.Write("\nУдаление первого элемента...");
+            deleteFirstItem = 0;
+            ref int deleteLastItem = ref delLastItem(myArray);
+            Console.Write("\nУдаление последнего элемента...");
+            deleteLastItem = 0;
+            ref int deleteYourItem = ref delItem(myArray);
+            Console.Write("\nУдаление элемента...");
+            deleteYourItem = 0;
+            printArray(myArray);
         }
     }
 }
