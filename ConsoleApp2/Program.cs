657d2eadeac64809408df9a0566f993c182e3a32
Bekzat <75122487+bekzat@users.noreply.github.com>
2020-12-14 00:01:22 +0600

    upd

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,8 +8,19 @@ namespace ConsoleApp2
 {
     class Program
     {
+        static void print(int[] array, int i=0)
+        {
+            if (i >= array.Length)
+                return;
+            Console.Write(array[i]+"\t");
+            i++;
+            print(array, i);
+        }
         static void Main(string[] args)
         {
+            int[] myArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            print(myArray);
+            Console.WriteLine();
         }
     }
 }
